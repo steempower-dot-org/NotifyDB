908ad1c29e7e4c6471ae04609f56e9d7e863bdd5
Gareth Nelson <gareth@garethnelson.com>
2016-11-10 06:28:29 +0000

    Simple blockloader

---
+++ blockloader.py
@@ -0,0 +1,23 @@
+""" Imports blockchain from a steemd node
+"""
+
+import config
+import json
+import requests
+import websocket
+
+ws = websocket.WebSocket()
+ws.connect(config.steemd_endpoint)
+ws.send('{"id":1,"method":"get_dynamic_global_properties","params":[]}')
+resp = ws.recv()
+
+resp_data = json.loads(resp)
+
+head_block_no = resp_data['result']['head_block_number']
+
+for x in xrange(head_block_no):
+    ws.send('{"id":%s,"method":"get_block","params":["%s"]}' % (x+1,x))
+    resp        = ws.recv()
+    resp_data   = json.loads(resp)['result']
+    upload_data = {'id':'$','number':str(x),'data':resp_data}
+    req         = requests.post('http://localhost:12036/blocks',json=upload_data)
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import steem as chain
 
 # memcache endpoint - you may actually specify multiple here to use a pool
 mc_endpoint = ['127.0.0.1:11211']
+
+# change this to specify a different node
+steemd_endpoint = 'ws://node.steem.ws'
